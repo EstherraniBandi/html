00039289d33231226b808207cbcae0c25bfe27a5
EstherraniBandi <bandiestherrani02@gmail.com>
2025-02-07 14:35:06 +0530

    Eventhandler

---
+++ firstjavascript.js
@@ -0,0 +1,11 @@
+// Variables
+let message = "Hello, World!";
+const PI = 3.14;
+
+// Function
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+// Output
+console.log(greet("dimple")); // Hello, Alice!
